59c02173a6b6c1e1c55722c864601bd56dab1f18
guallo <eacuesta1986@gmail.com>
2019-05-29 01:07:32 -0400

    added support for HUB_ADDR environment variable

--- tr.py
+++ tr.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python
 
+import os
 import sys
 import socket
 
+DEFAULT_HUB_ADDR = 'localhost:8091'
+
+
+hub_addr = os.environ.get('HUB_ADDR', DEFAULT_HUB_ADDR)
+hub_host = hub_addr.split(':')[0]
+hub_port = int(hub_addr.split(':')[1])
 
 # sending...
 str_ = sys.argv[1]
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('localhost', 8090))
+s.connect((hub_host, hub_port))
 s.sendall(str_ + '\0')
 
 # receiving...
